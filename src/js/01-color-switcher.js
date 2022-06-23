d7312f7a4fc4b6e490f37c0579fe635371bb9eba
Vlasenko Aleksandr <baralexx181972@gmail.com>
2022-06-23 17:08:29 +0300

    hw-9-done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,39 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+addDisabledAtr(refs.stopBtn);
+
+refs.startBtn.addEventListener('click', onClickStartColorChange);
+
+refs.stopBtn.addEventListener('click', onClickStopColorChange);
+
+function onClickStartColorChange() {
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  removeDisabledAtr(refs.stopBtn);
+  addDisabledAtr(refs.startBtn);
+}
+
+function onClickStopColorChange() {
+  clearInterval(timerId);
+
+  removeDisabledAtr(refs.startBtn);
+  addDisabledAtr(refs.stopBtn);
+}
+
+function addDisabledAtr(linkOnBtn) {
+  linkOnBtn.setAttribute('disabled', 'disabled');
+}
+
+function removeDisabledAtr(linkOnBtn) {
+  linkOnBtn.removeAttribute('disabled', 'disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix';
+
+const formRef = document.querySelector('.form');
+formRef.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  const { delay, step, amount } = e.target.elements;
+
+  let delayCount = Number(delay.value);
+  for (i = 1; i <= amount.value; i += 1) {
+    createPromise(i, delayCount)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, {
+          useIcon: false,
+        });
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, {
+          useIcon: false,
+        });
+      });
+
+    delayCount += Number(step.value);
   }
+  e.target.reset();
 }
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+g;
